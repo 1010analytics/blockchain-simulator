1d586cd0232fd2f68371da623d9c47faa6a066e2
1010analytics <tathya.joshipura@1010analytics.io>
2024-04-30 02:51:10 +0530

    Implemented POS and Smart contracts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::Write;
 use sha2::{Sha256, Digest};
@@ -9,6 +10,20 @@ struct Transaction {
     sender: String,
     receiver: String,
     amount: f64,
+    payload: String, 
+}
+
+#[derive(Debug, Clone)]
+struct SmartContract {
+    code: String, 
+}
+
+impl SmartContract {
+    fn execute(&self, data: &Transaction) -> f64 {
+        
+        println!("Executing contract code: {}", self.code);
+        data.amount * 2.0 
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -17,15 +32,17 @@ struct Block {
     transactions: Vec<Transaction>,
     hash: String,
     nonce: u64,
+    validator: String,
 }
 
 impl Block {
-    fn new(transactions: Vec<Transaction>, previous_hash: String) -> Block {
+    fn new(transactions: Vec<Transaction>, previous_hash: String, validator: String) -> Block {
         let mut block = Block {
             previous_hash,
             transactions,
             hash: String::new(),
             nonce: 0,
+            validator,
         };
         block.hash = block.calculate_hash();
         block
@@ -35,41 +52,44 @@ impl Block {
         let mut hasher = Sha256::new();
         hasher.update(&self.previous_hash);
         hasher.update(&self.nonce.to_string());
+        hasher.update(&self.validator);
         for transaction in &self.transactions {
             hasher.update(&transaction.sender);
             hasher.update(&transaction.receiver);
             hasher.update(&transaction.amount.to_string());
+            hasher.update(&transaction.payload);
         }
         format!("{:x}", hasher.finalize())
-    }
-
-    fn mine(&mut self, difficulty: usize) {
-        let target = std::iter::repeat("0").take(difficulty).collect::<String>();
-        while &self.hash[..difficulty] != target {
-            self.nonce += 1;
-            self.hash = self.calculate_hash();
-        }
     }
 }
 
 #[derive(Debug)]
 struct Blockchain {
     blocks: Vec<Block>,
+    stakes: HashMap<String, f64>,
+    contracts: Vec<SmartContract>,
 }
 
 impl Blockchain {
     fn new() -> Blockchain {
-        let genesis_block = Block::new(vec![], "0".to_string());
+        let genesis_block = Block::new(vec![], "0".to_string(), "genesis_validator".to_string());
         let mut blockchain = Blockchain {
             blocks: vec![genesis_block],
+            stakes: HashMap::new(),
+            contracts: vec![SmartContract { code: "return input * 2;".to_string() }], 
         };
-        blockchain.blocks[0].mine(4); 
+        blockchain.stakes.insert("genesis_validator".to_string(), 1000.0);
         blockchain
     }
 
     fn add_block(&mut self, mut new_block: Block) {
-        new_block.mine(4); 
-        self.blocks.push(new_block);
+        if self.validate_block(&new_block) {
+            self.blocks.push(new_block);
+        }
+    }
+
+    fn validate_block(&self, block: &Block) -> bool {
+        self.stakes.get(&block.validator).map_or(false, |&stake| stake > 100.0)
     }
 }
 
@@ -91,6 +111,7 @@ fn main() {
         let sender = read_input("Enter sender: ");
         let receiver = read_input("Enter receiver: ");
         let amount = read_input("Enter amount: ");
+        let payload = read_input("Enter payload or contract call: ");
         let amount: f64 = amount.parse().expect("Expected a number for amount");
 
         thread::spawn(move || {
@@ -98,9 +119,10 @@ fn main() {
                 sender,
                 receiver,
                 amount,
+                payload,
             };
 
-            let block = Block::new(vec![transaction], blockchain.lock().unwrap().blocks.last().unwrap().hash.clone());
+            let block = Block::new(vec![transaction], blockchain.lock().unwrap().blocks.last().unwrap().hash.clone(), "some_validator_address".to_string());
             blockchain.lock().unwrap().add_block(block);
         });
 
